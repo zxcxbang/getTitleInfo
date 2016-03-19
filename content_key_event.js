87e94904812bcd427fbdde3e833973095d838369
allen.bang <allen.bang@gmail.com>
2016-03-19 15:54:19 +0800

    trim spaces in which title

--- content_key_event.js
+++ content_key_event.js
@@ -7,7 +7,7 @@ function doKeyPress(e){
     if (e.ctrlKey && e.keyCode == trigger_key){
         //alert('数据已拷贝到剪贴板')
         //chrome.extension.sendRequest({redirect: newurl});
-        var title = document.getElementsByTagName("title")[0].innerHTML;
+        var title = document.getElementsByTagName("title")[0].innerHTML.trim();
         var url = window.location.href
         var message = title + "\n" + url
         var now = new Date()
